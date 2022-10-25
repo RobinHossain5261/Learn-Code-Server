c188263b2f72fcbe3e7aaeb42cb4e1465d79ed69
Robin Hossain <robinhossain5261@gmail.com>
2022-10-25 23:09:14 +0600

    dynamic course added

--- index.js
+++ index.js
@@ -5,9 +5,27 @@ app.use(cors());
 
 const port = process.env.port || 5000;
 
+const courses = require('./data/courses.json');
+const category = require('./data/category.json');
+
 app.get('/', (req, res) => {
     res.send('Now server is running');
 });
+
+app.get('/courses', (req, res) => {
+    res.send(courses);
+})
+
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const selectCourse = courses.find(course => course.id == id);
+    if (!selectCourse) {
+        res.send('Course is not found');
+    }
+    res.send(selectCourse);
+})
+
+
 
 
 app.listen(port, () => {
